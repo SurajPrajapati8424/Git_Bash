479975f6ea18adbce9e72f25407ebce61046150b
AndroidStudio <surajprajapati8286@gmail.com>
2021-02-09 04:19:47 +0530

    NonSwipableView | No Error

--- app/src/main/java/com/dr/psycho/ryuk/Utils/NonSwipableViewPage.java
+++ app/src/main/java/com/dr/psycho/ryuk/Utils/NonSwipableViewPage.java
@@ -13,6 +13,17 @@ import androidx.viewpager.widget.ViewPager;
 import java.lang.reflect.Field;
 
 public class NonSwipableViewPage extends ViewPager {
+
+    public NonSwipableViewPage(@NonNull Context context) {
+        super(context);
+        setMyScroll();
+    }
+
+    public NonSwipableViewPage(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        setMyScroll();
+    }
+
     //Ctrl+O
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
@@ -24,27 +35,18 @@ public class NonSwipableViewPage extends ViewPager {
         return false;
     }
 
-    public NonSwipableViewPage(@NonNull Context context) {
-        super(context);
-        setMyScroll();
-    }
-
     private void setMyScroll() {
         try {
             Class<?> viewpager = ViewPager.class;
             Field scroller = viewpager.getDeclaredField("mScroller");
             scroller.setAccessible(true);
-            scroller.set(this,new MyScroller(getContext()));
+            scroller.set(this, new MyScroller(getContext()));
         }
         catch (NoSuchFieldException | IllegalAccessException e){
             e.printStackTrace();
         }
     }
 
-    public NonSwipableViewPage(@NonNull Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-        setMyScroll();
-    }
 
     private class MyScroller extends Scroller {
         public MyScroller(Context context) {
@@ -52,20 +54,6 @@ public class NonSwipableViewPage extends ViewPager {
 
         }
 
-        /**
-         * Start scrolling by providing a starting point, the distance to travel,
-         * and the duration of the scroll.
-         *
-         * @param startX   Starting horizontal scroll offset in pixels. Positive
-         *                 numbers will scroll the content to the left.
-         * @param startY   Starting vertical scroll offset in pixels. Positive numbers
-         *                 will scroll the content up.
-         * @param dx       Horizontal distance to travel. Positive numbers will scroll the
-         *                 content to the left.
-         * @param dy       Vertical distance to travel. Positive numbers will scroll the
-         *                 content up.
-         * @param duration Duration of the scroll in milliseconds.
-         */
         @Override
         public void startScroll(int startX, int startY, int dx, int dy, int duration) {
             super.startScroll(startX, startY, dx, dy, 400);
